25b5d44426539ffa03d643f5efe48861697f81ae
Angel Doria <danieldoriamoncada28@gmail.com>
2024-02-06 14:06:12 -0600

    Tarea Finalizada

--- src/app/countries/pages/by-country-page/by-country-page.component.ts
+++ src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/country';
+import { CountriesService } from '../../services/countries.service';
 
 @Component({
   selector: 'app-by-country-page',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class ByCountryPageComponent {
+
+  public countries: Country[] = [];
+
+  constructor(private CountriesService: CountriesService){}
+
+  searchByCountry(term:string):void {
+    this.CountriesService.searchCountry(term)
+      .subscribe(countries => {
+        this.countries = countries;
+      });
+  }
 
 }
