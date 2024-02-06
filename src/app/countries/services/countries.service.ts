6801e387d7fa2600cf9475d4969c90bd4c2e73ee
Angel Doria <danieldoriamoncada28@gmail.com>
2024-02-06 15:29:39 -0600

    Seccion 9 Finalizada

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,7 +1,7 @@
 
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 
 import { Country } from '../interfaces/country';
 
@@ -13,6 +13,17 @@ export class CountriesService {
   private apiUrl: string = 'https://restcountries.com/v3.1'
 
   constructor(private http: HttpClient) { }//Mediante esto hacemos la peticion a la API
+
+  searchCountryByAlphaCode(code:string): Observable<Country | null> {
+
+    const url = `${this.apiUrl}/alpha/${code}`;
+
+    return this.http.get<Country[]>( url )
+     .pipe(
+      map( countries => countries.length > 0 ? countries[0] : null ), //Si hay mas de un país con ese código devuelve solo uno
+      catchError( () => of(null) )
+     );
+  }
 
   searchCapital(term: string): Observable<Country[]> {//Funcion que se encarga de hacer la peticion a la API mediante el termino de busqueda en base a la interfaz
     const url = `${this.apiUrl}/capital/${term}`;
